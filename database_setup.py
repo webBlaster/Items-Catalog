c303bcc61e468045fa58324ff34841a136f505ed
Yekinni Ajibola <yekinnijibola@gmail.com>
2019-09-27 16:14:44 +0100

    Added db

--- database_setup.py
+++ database_setup.py
@@ -49,5 +49,5 @@ class Item(Base):
 
 
 # engine = create_engine('postgresql://catalog:catalog@localhost/catalog')
-engine = create_engine('sqlite:///restaurantmenu')
+engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.create_all(engine)
